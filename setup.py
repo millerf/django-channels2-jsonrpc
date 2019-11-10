b5f0ffbaf5daeea7178cffc92eacd3f91ac9ccdb
Fabien MILLERAND <fab@millerf.com>
2019-11-10 20:19:12 +0100

    v 1.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-channels2-jsonrpc',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     install_requires=[
           'channels'
